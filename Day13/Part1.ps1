aa0945c25ea90bad45a4f01fb9682d046742fa75
Mike Beemsterboer <m.c.p.beemsterboer@dnb.nl>
2022-12-13 21:00:28 +0100

    :package: Day 13

--- Day13/Part1.ps1
+++ Day13/Part1.ps1
@@ -1,23 +1,24 @@
-$stopwatch = [System.Diagnostics.Stopwatch]::new()
-$Stopwatch.Start()
-
 #Change if you run this on another day
 $day=(Get-Date).Day
 $Year=(Get-Date).Year
 
 #Comment next line for run with input
-[System.Collections.ArrayList]$puzzleInput = (Get-Content ./Day$day/input.txt) | Where-Object {
-    $_.Trim() -ne ""
-}
-$puzzleInput.Add("[[2]]") | Out-Null
-$puzzleInput.Add("[[6]]") | Out-Null
-$puzzleInput = $puzzleInput | Sort-Object
-
-function rightOrder ($leftList, $rightList){
+#$puzzleInput = (Get-Content ./Day$day/sample.txt -Raw) -split '(?:\r?\n){2,}'
+$puzzleInput = (Get-Content ./Day$day/input.txt -Raw) -split '(?:\r?\n){2,}'
+$pairIndex=0
+$rightOrdered=@()
+foreach($pair in $puzzleInput){
+    $pairIndex++
+    #$pair = $puzzleInput[0]
+    #$pair = $puzzleInput[15]
+    $leftList, $rightList = $pair.Split([System.Environment]::NewLine)
     [System.Collections.ArrayList]$leftPacket = $leftList.Replace(10,"A").ToCharArray()
     [System.Collections.ArrayList]$rightPacket = $rightList.Replace(10,"A").ToCharArray()
 
+
+    #$item = $left[0]
     for($i=0; $i -lt [Math]::Min($leftPacket.Count,$rightPacket.Count); $i++){
+        #$i++
         $left = $leftPacket[$i]
         $right = $rightPacket[$i]
         if($left -eq $right){
@@ -26,10 +27,11 @@ function rightOrder ($leftList, $rightList){
         }
         if([int]$left -in 48..65 -and [int]$right -in 48..65){
             if(($right-$left) -ge 1) {
-                return $true
+                $rightOrdered += $pairIndex
+                break
             }
             elseif(($right-$left) -lt 0){
-                return $false
+                break
             }
         }
         #If left or right numeric and the other is a list, change 1 to [1]
@@ -46,30 +48,13 @@ function rightOrder ($leftList, $rightList){
         #If left closes before right is closed
         if($left -eq "]" -and $right -ne "]"){
             #Left runned out of items, order is correct
-            return $true
+            $rightOrdered += $pairIndex
+            break
         }
         if($right -eq "]" -and $left -ne "]"){
             #Right runned out of items, order is not correct
-            return $false
+            break
         }
     }
 }
-
-for($inputIndex=0; $inputIndex -lt ($puzzleInput.Count-1); $inputIndex++){
-    if(-not (rightOrder $puzzleInput[$inputIndex] $puzzleInput[$inputIndex+1])){
-        $toMove = $puzzleInput[$inputIndex+1]
-        $puzzleInput.RemoveAt($inputIndex+1)
-        $puzzleInput.Insert($inputIndex, $toMove)
-        #start from the beginning again
-        $inputIndex=-1
-    }
-    if($puzzleInput[$inputIndex] -eq "[[2]]"){
-        $decoder = ($inputIndex+1)
-    }
-    if($puzzleInput[$inputIndex] -eq "[[6]]"){
-        $decoder = $decoder * ($inputIndex+1)
-    }
-}
-$decoder
-$Stopwatch.Stop()
-write-host ("That took {0} Milliseconds, {1} seconds" -f $Stopwatch.Elapsed.TotalMilliseconds, $stopwatch.Elapsed.TotalSeconds)
+$rightOrdered | Measure-Object -Sum
