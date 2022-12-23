d99a984182d11bed9cb5f7855a926a2384cb15d8
Mike Beemsterboer <m.c.p.beemsterboer@dnb.nl>
2022-12-23 15:07:18 +0100

    :seedling: Day 23

---
+++ Day23/Part2.ps1
@@ -0,0 +1,103 @@
+$stopwatch = [System.Diagnostics.Stopwatch]::new()
+$Stopwatch.Start()
+
+#Change if you run this on another day
+$day=23
+
+#Comment next line for run with input
+#$puzzleInput = (Get-Content ./Day$day/sample.txt)
+$puzzleInput = (Get-Content ./Day$day/input.txt)
+
+$elves=[System.Collections.ArrayList]@()
+for($y=0; $y -lt $puzzleInput.Count; $y++){
+    for ($x = 0; $x -lt $puzzleInput[0].Length; $x++) {
+        if($puzzleInput[$y][$x] -eq "#"){
+            $elves.Add("$x,$y") | Out-Null
+        }
+    }
+}
+
+$directions = [System.Collections.ArrayList]@("N","S","W","E")
+$round=0
+$stop = $false
+while($stop -eq $false){
+    $round++
+    $freeElves=0
+    $proposals = @{}
+    foreach($elf in $elves){
+        #Start proposal with current, then look to every direction to change it
+        $proposals[$elf] = $elf
+        [int]$x, [int]$y = $elf.Split(",")
+        $free = @()
+        foreach($direction in $directions){
+            switch ($direction) {
+                "N" {
+                    $look = ("{1},{0}:{2},{0}:{3},{0}" -f ($y-1),($x-1),$x,($x+1)).Split(":")
+                    if($elves.IndexOf($look[0]) -eq -1 -and $elves.IndexOf($look[1]) -eq -1 -and $elves.IndexOf($look[2]) -eq -1){
+                        #Found north, next elve
+                        $free += $direction
+                    }
+                }
+                "S" {
+                    $look = ("{1},{0}:{2},{0}:{3},{0}" -f ($y+1),($x-1),$x,($x+1)).Split(":")
+                    if($elves.IndexOf($look[0]) -eq -1 -and $elves.IndexOf($look[1]) -eq -1 -and $elves.IndexOf($look[2]) -eq -1){
+                        #Found south, next elve
+                        $free += $direction
+                    }
+                 }
+                "W" {
+                    $look = ("{0},{1}:{0},{2}:{0},{3}" -f ($x-1),($y-1),$y,($y+1)).Split(":")
+                    if($elves.IndexOf($look[0]) -eq -1 -and $elves.IndexOf($look[1]) -eq -1 -and $elves.IndexOf($look[2]) -eq -1){
+                        #Found west, next elve
+                        $free += $direction
+                    }
+                 }
+                "E" {
+                    $look = ("{0},{1}:{0},{2}:{0},{3}" -f ($x+1),($y-1),$y,$($y+1)).Split(":")
+                    if($elves.IndexOf($look[0]) -eq -1 -and $elves.IndexOf($look[1]) -eq -1 -and $elves.IndexOf($look[2]) -eq -1){
+                        #Found east, next elve
+                        $free += $direction
+                    }
+                 }
+            }
+
+        }
+        if($free.Count -gt 0 -and $free.Count -lt 4){
+            $direction = $free[0]
+            switch ($direction) {
+                "N" { $proposals["$x,$y"] = ("{0},{1}" -f $x,($y-1)) }
+                "S" { $proposals["$x,$y"] = ("{0},{1}" -f $x,($y+1)) }
+                "W" { $proposals["$x,$y"] = ("{0},{1}" -f ($x-1),$y) }
+                "E" { $proposals["$x,$y"] = ("{0},{1}" -f ($x+1),$y) }
+            }
+        }
+        if($free.Count -eq 4){
+            $freeElves++
+        }
+    }
+
+    # Look for double proposals
+    $doubles = $proposals.GetEnumerator() | Group-Object Value | Where-Object { $_.Count -gt 1 }
+    if($null -ne $doubles){
+        $doubles.Group | ForEach-Object {
+            $proposals[$_.Name] = $_.Name
+        }
+    }
+
+    if($freeElves -eq $elves.Count){
+        $round
+        $stop = $true
+    }
+    # Repopulate $elves
+    $elves=[System.Collections.ArrayList]@()
+    foreach($elf in $proposals.Values){
+        $elves.Add($elf) | Out-Null
+    }
+
+    #switch directions for next round
+    $toEnd = $directions[0]
+    $directions.RemoveAt(0)
+    $directions.Add($toEnd) | Out-Null
+}
+$Stopwatch.Stop()
+write-host ("That took {0} Milliseconds, {1} seconds" -f $Stopwatch.Elapsed.TotalMilliseconds, $stopwatch.Elapsed.TotalSeconds)
